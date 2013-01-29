9b3793f36edabf80b0308c43bbbee7b1b94805d1
Jay Wang <jayduke15@gmail.com>
2013-01-29 17:01:59 -0500

    added fixedmass.java

---
+++ src/simulation/FixedMass.java
@@ -0,0 +1,15 @@
+package simulation;
+
+import java.awt.Dimension;
+
+public class FixedMass extends Mass {
+
+	public FixedMass(double x, double y, double mass) {
+		super(x, y, mass);
+		// TODO Auto-generated constructor stub
+	}
+	
+    public void update (double elapsedTime, Dimension bounds) {
+    }
+
+}
